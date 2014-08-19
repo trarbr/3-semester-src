62cee79dce6147788a45e52ff8a435729bb66f4b
Troels Brødsgaard <trarbr@gmail.com>
2014-08-19 13:30:12 +0200

    add SocketOpgave2

---
+++ SocketOpgave2/SimpleClient/SimpleClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClient
+{
+    class SimpleClient
+    {
+        private string serverIP;
+        private int serverPort;
+
+        public SimpleClient(string serverIP, int serverPort)
+        {
+            this.serverIP = serverIP;
+            this.serverPort = serverPort;
+        }
+
+        internal void Run()
+        {
+            TcpClient server = new TcpClient(serverIP, serverPort);
+
+            NetworkStream networkStream = server.GetStream();
+            StreamWriter writer = new StreamWriter(networkStream);
+            StreamReader reader = new StreamReader(networkStream);
+
+            Console.WriteLine("Server says " + reader.ReadLine());
+
+            string userInput = Console.ReadLine();
+
+            writer.WriteLine(userInput);
+            writer.Flush();
+
+            Console.WriteLine("Server says " + reader.ReadLine());
+
+            reader.Close();
+            writer.Close();
+            networkStream.Close();
+            server.Close();
+
+        }
+    }
+}
---
+++ SocketOpgave2/SimpleServer/SimpleServer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleServer
+{
+    class SimpleServer
+    {
+        private IPAddress serverIP;
+        private int serverPort;
+
+        public SimpleServer(IPAddress serverIP, int serverPort)
+        {
+            this.serverIP = serverIP;
+            this.serverPort = serverPort;
+        }
+
+        public void Run()
+        {
+            TcpListener listener = new TcpListener(serverIP, serverPort);
+
+            listener.Start();
+
+            Console.WriteLine("Now listening on " + serverIP + " " + serverPort);
+
+            Socket client = listener.AcceptSocket();
+
+            IPEndPoint clientEndPoint = (IPEndPoint)client.RemoteEndPoint;
+            Console.WriteLine("Client connected! " + clientEndPoint.Address + " " + clientEndPoint.Port);
+
+            NetworkStream networkStream = new NetworkStream(client);
+            StreamWriter writer = new StreamWriter(networkStream);
+            StreamReader reader = new StreamReader(networkStream);
+
+            writer.WriteLine("Ready");
+            writer.Flush();
+
+            string input = reader.ReadLine();
+
+            if (input == "time?")
+            {
+                writer.WriteLine(String.Format("{0:HH:mm:ss}", DateTime.Now));
+                writer.Flush();
+            }
+            else
+            {
+                writer.WriteLine("Unkown command");
+                writer.Flush();
+            }
+
+            reader.Close();
+            writer.Close();
+            networkStream.Close();
+            client.Close();
+        }
+
+    }
+}
