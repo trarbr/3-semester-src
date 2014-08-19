93381a28023532a3a49e26fbc79cc3c891a3e1ca
Troels Brødsgaard <trarbr@gmail.com>
2014-08-19 13:42:38 +0200

    add SocketOpgave3

---
+++ SocketOpgave3/SimpleClient/SimpleClient.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClient
+{
+    class SimpleClient
+    {
+        private string serverIP;
+        private int serverPort;
+
+        public SimpleClient(string serverIP, int serverPort)
+        {
+            this.serverIP = serverIP;
+            this.serverPort = serverPort;
+        }
+
+        internal void Run()
+        {
+            TcpClient server = new TcpClient(serverIP, serverPort);
+
+            NetworkStream networkStream = server.GetStream();
+            StreamWriter writer = new StreamWriter(networkStream);
+            StreamReader reader = new StreamReader(networkStream);
+
+            Console.WriteLine("Server says " + reader.ReadLine());
+
+            Console.Write("Tell server: ");
+            string userInput = Console.ReadLine();
+            while (userInput != null && userInput.Trim().ToLower() != "exit")
+            {
+                writer.WriteLine(userInput);
+                writer.Flush();
+
+                Console.WriteLine("Server says " + reader.ReadLine());
+
+                Console.Write("Tell server: ");
+                userInput = Console.ReadLine();
+            }
+
+            writer.WriteLine("exit");
+            writer.Flush();
+
+            reader.Close();
+            writer.Close();
+            networkStream.Close();
+            server.Close();
+
+        }
+    }
+}
---
+++ SocketOpgave3/SimpleServer/SimpleServer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleServer
+{
+    class SimpleServer
+    {
+        private IPAddress serverIP;
+        private int serverPort;
+
+        public SimpleServer(IPAddress serverIP, int serverPort)
+        {
+            this.serverIP = serverIP;
+            this.serverPort = serverPort;
+        }
+
+        public void Run()
+        {
+            TcpListener listener = new TcpListener(serverIP, serverPort);
+
+            listener.Start();
+
+            Console.WriteLine("Now listening on " + serverIP + " " + serverPort);
+
+            Socket client = listener.AcceptSocket();
+
+            IPEndPoint clientEndPoint = (IPEndPoint)client.RemoteEndPoint;
+            Console.WriteLine("Client connected! " + clientEndPoint.Address + " " + clientEndPoint.Port);
+
+            NetworkStream networkStream = new NetworkStream(client);
+            StreamWriter writer = new StreamWriter(networkStream);
+            StreamReader reader = new StreamReader(networkStream);
+
+            writer.WriteLine("Ready");
+            writer.Flush();
+
+            bool clientConnected = true;
+
+            while (clientConnected)
+            {
+                string input = reader.ReadLine().Trim().ToLower();
+
+                if (input == "time?")
+                {
+                    writer.WriteLine(String.Format("{0:HH:mm:ss}", DateTime.Now));
+                    writer.Flush();
+                }
+                else if (input == "date?")
+                {
+                    writer.WriteLine(String.Format("{0:yyyy-MM-dd}", DateTime.Now));
+                    writer.Flush();
+                }
+                else if (input == "exit")
+                {
+                    Console.WriteLine("Client disconnected");
+                    clientConnected = false;
+                }
+                else
+                {
+                    writer.WriteLine("Unkown command");
+                    writer.Flush();
+                }
+            }
+
+            reader.Close();
+            writer.Close();
+            networkStream.Close();
+            client.Close();
+        }
+    }
+}
