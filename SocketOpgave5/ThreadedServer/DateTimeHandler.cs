8998eabde2821c44f66264dc5e1cb68f9ff8b83e
Troels Brødsgaard <trarbr@gmail.com>
2014-08-20 10:42:12 +0200

    add SocketOpgave5: basic implementation

---
+++ SocketOpgave5/ThreadedServer/DateTimeHandler.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadedServer
+{
+    class DateTimeHandler
+    {
+        private Socket client;
+
+        public DateTimeHandler(Socket client)
+        {
+            this.client = client;
+        }
+
+        public void Handle()
+        {
+            IPEndPoint clientEndPoint = (IPEndPoint)client.RemoteEndPoint;
+            Console.WriteLine(String.Format("Client connected! IP: {0} Port: {1}",
+                clientEndPoint.Address, clientEndPoint.Port));
+
+            NetworkStream networkStream = new NetworkStream(client);
+            StreamWriter writer = new StreamWriter(networkStream);
+            StreamReader reader = new StreamReader(networkStream);
+
+            writer.WriteLine("Ready");
+            writer.Flush();
+
+            bool clientConnected = true;
+
+            while (clientConnected)
+            {
+                string input = reader.ReadLine().Trim().ToLower();
+
+                switch (input)
+                {
+                    case "time?":
+                        writer.WriteLine(String.Format("{0:HH:mm:ss}", DateTime.Now));
+                        writer.Flush();
+                        break;
+                    case "date?":
+                        writer.WriteLine(String.Format("{0:yyyy-MM-dd}", DateTime.Now));
+                        writer.Flush();
+                        break;
+                    case "exit":
+                        Console.WriteLine("Client disconnected");
+                        clientConnected = false;
+                        break;
+                    default:
+                        writer.WriteLine("Unkown command");
+                        writer.Flush();
+                        break;
+                }
+            }
+
+            reader.Close();
+            writer.Close();
+            networkStream.Close();
+            client.Close();
+        }
+    }
+}
---
+++ SocketOpgave5/ThreadedServer/ThreadedServer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadedServer
+{
+    class ThreadedServer
+    {
+        private IPAddress serverIP;
+        private int serverPort;
+
+        public ThreadedServer(IPAddress serverIP, int serverPort)
+        {
+            this.serverIP = serverIP;
+            this.serverPort = serverPort;
+        }
+
+        public void Serve()
+        {
+            TcpListener listener = new TcpListener(serverIP, serverPort);
+
+            listener.Start();
+
+            Console.WriteLine(String.Format("Now listening on IP {0} and port {1}",
+                serverIP, serverPort));
+
+            while (true)
+            {
+                Socket client = listener.AcceptSocket();
+
+                DateTimeHandler handler = new DateTimeHandler(client);
+                ThreadStart starter = new ThreadStart(handler.Handle);
+                Thread thread = new Thread(starter);
+
+                thread.Start();
+            }
+        }
+    }
+}
