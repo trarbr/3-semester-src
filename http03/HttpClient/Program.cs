27f6d7df8993c4e1429ddef379e216282cd806a5
Troels Brødsgaard <tr@arbr.dk>
2014-08-26 12:18:05 +0200

    http03: add

---
+++ http03/HttpClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient client = new WebClient();
+
+            string besked = client.DownloadString("http://webservicedemo.datamatiker-skolen.dk/RegneWcfService.svc/RESTjson/Add?a=5&b=8");
+            Console.WriteLine(besked);
+
+            Console.ReadLine();
+
+        }
+    }
+}
