bd28468d22d753ec5a1ff3d54805656a60ccd284
Troels Brødsgaard <tr@arbr.dk>
2014-08-27 10:39:11 +0200

    threads03: add

---
+++ threads03/threads03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace threads03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thermo thermo = new Thermo();
+            Thread thread = new Thread(thermo.Run);
+
+            thread.Start();
+
+            while (thread.IsAlive)
+            {
+                Thread.Sleep(10000);
+                Console.WriteLine("Thread is alive");
+            }
+
+            Console.WriteLine("Alarm thread terminated");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ threads03/threads03/Thermo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace threads03
+{
+    class Thermo
+    {
+
+        public void Run()
+        {
+            int alarmCounter = 0;
+            while (alarmCounter < 3)
+            {
+                int temperature = generateTemperature();
+                Console.WriteLine("Temperature: {0}", temperature);
+                if (temperature < 0 || temperature > 100)
+                {
+                    Console.WriteLine("Alarm");
+                    alarmCounter++;
+                }
+
+                Thread.Sleep(2000);
+            }
+        }
+
+        private int generateTemperature()
+        {
+            Random generator = new Random();
+
+            int temperature = generator.Next(-20, 120);
+
+            return temperature;
+        }
+    }
+}
