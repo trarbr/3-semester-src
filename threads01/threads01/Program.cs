0ae56cb682ad46401a803b948aa699ea972a044f
Troels Brødsgaard <tr@arbr.dk>
2014-08-27 10:09:40 +0200

    threads01: add

---
+++ threads01/threads01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace threads01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Repeater repeater = new Repeater();
+            Thread thread = new Thread(repeater.Run);
+            thread.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ threads01/threads01/Repeater.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace threads01
+{
+    class Repeater
+    {
+        public void Run()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("C# trådning er nemt!");
+            }
+
+        }
+    }
+}
