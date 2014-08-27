9d902da34b4384f999115428004af91ab39ad003
Troels Brødsgaard <tr@arbr.dk>
2014-08-27 12:05:59 +0200

    threads05: add - also solves threads06

---
+++ threads05/threads05/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace threads05
+{
+    class Program
+    {
+        static int counter;
+
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(printStars);
+            Thread thread2 = new Thread(printHashes);
+
+            thread1.Start();
+            thread2.Start();
+
+            Console.ReadLine();
+        }
+
+        static void printStars()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                lock ((object)counter)
+                {
+                    counter += 60;
+                    Console.WriteLine("************************************************************ " + counter);
+                    Thread.Sleep(100);
+                }
+            }
+        }
+
+        static void printHashes()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                lock ((object)counter)
+                {
+                    counter += 60;
+                    Console.WriteLine("############################################################ " + counter);
+                    Thread.Sleep(100);
+                }
+            }
+        }
+
+    }
+}
