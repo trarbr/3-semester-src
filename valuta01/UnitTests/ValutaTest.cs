05085cff742637d99d2ac262c088c8c3fe0a689c
Troels Brødsgaard <tr@arbr.dk>
2014-09-30 08:16:07 +0200

    valuta01: add

---
+++ valuta01/UnitTests/ValutaTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ValutaWcfService;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ValutaTest
+    {
+        [TestMethod]
+        public void Test_FromDkkToEur()
+        {
+            ValutaService service = new ValutaService();
+            decimal expectedEur = 13.40500542902719875601549619m;
+
+            decimal actualEur = service.FromDkkToEur(100);
+
+            Assert.AreEqual(expectedEur, actualEur);
+        }
+    }
+}
---
+++ valuta01/ValutaConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValutaConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ValutaWcfService.IValutaService valutaService = new ValutaWcfService.ValutaServiceClient();
+
+            Console.Write("Amount of DKK to convert to EUR: ");
+            string amountAsString = Console.ReadLine();
+            decimal dkkAmount = decimal.Parse(amountAsString);
+
+            decimal eurAmount = valutaService.FromDkkToEur(100);
+
+            Console.WriteLine("In EUR, that is " + eurAmount.ToString("N2"));
+
+            Console.ReadLine();
+        }
+    }
+}
