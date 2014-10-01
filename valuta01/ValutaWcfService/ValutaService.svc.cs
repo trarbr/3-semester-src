668a2261fdc6ef177a6042b6aa896ff097ef9956
Troels Brødsgaard <tr@arbr.dk>
2014-10-01 09:28:00 +0200

    valuta01: lock on all public methods

--- valuta01/ValutaWcfService/ValutaService.svc.cs
+++ valuta01/ValutaWcfService/ValutaService.svc.cs
@@ -53,7 +53,9 @@ namespace ValutaWcfService
             }
         }
 
-        // use this to lock when changing the lists
+        // use this as a coarse grained lock
+        // could have one coarse grained lock and one lock for each of valutas and conversions
+        // might be useful for reducing the scope of each lock, but also a hassle to implement correctly
         private object serviceLock; 
 
         public ValutaService()
@@ -63,58 +65,86 @@ namespace ValutaWcfService
 
         public decimal FromDkkToEur(decimal dkkAmount)
         {
-            decimal dkkToEurRate = findExchangeRate("EUR");
-            decimal euroAmount = dkkAmount / dkkToEurRate * 100;
+            decimal euroAmount = 0m;
+            lock (serviceLock)
+            {
+                decimal dkkToEurRate = findExchangeRate("EUR");
+                euroAmount = dkkAmount / dkkToEurRate * 100;
+            }
 
             return euroAmount;
         }
 
         public decimal GetExchangeRate(string iso)
         {
-            decimal exchangeRate = findValuta(iso).ExchangeRate;
+            decimal exchangeRate;
+            lock (serviceLock)
+            {
+                exchangeRate = findExchangeRate(iso);
+            }
 
             return exchangeRate;
         }
 
         public Valuta[] GetValutas()
         {
-            return valutas.ToArray();
+            Valuta[] valutasArray;
+            lock (serviceLock)
+            {
+                valutasArray = valutas.ToArray();
+            }
+
+            return valutasArray;
         }
 
         public decimal ConvertFromIsoToIso(string fromIso, string toIso, decimal amount)
         {
             decimal newAmount;
-
-            try
+            lock (serviceLock)
             {
-                newAmount = amount * findExchangeRate(fromIso) / findExchangeRate(toIso);
-            }
-            catch (DivideByZeroException)
-            {
-                newAmount = 0m;
-            }
+                try
+                {
+                    newAmount = amount * findExchangeRate(fromIso) / findExchangeRate(toIso);
+                }
+                catch (DivideByZeroException)
+                {
+                    newAmount = 0m;
+                }
 
-            conversions.Add(String.Format("{0} {1} {2} {3}", 
-                amount.ToString("N2"), fromIso, newAmount.ToString("N2"), toIso));
-            HttpContext.Current.Session["conversions"] = conversions;
+                conversions.Add(String.Format("{0} {1} {2} {3}",
+                    amount.ToString("N2"), fromIso, newAmount.ToString("N2"), toIso));
+                HttpContext.Current.Session["conversions"] = conversions;
+            }
 
             return newAmount;
         }
 
         public string[] GetDoneConversions()
         {
-            return conversions.ToArray();
+            string[] conversionsArray;
+            lock (serviceLock)
+            {
+                conversionsArray = conversions.ToArray();
+            }
+
+            return conversionsArray;
         }
 
         public void SetValutaExchangeRate(Valuta valuta)
         {
-            Valuta actualValuta = findValuta(valuta.Iso);
-            actualValuta.ExchangeRate = valuta.ExchangeRate;
+            lock (serviceLock)
+            {
+                Valuta actualValuta = findValuta(valuta.Iso);
+                actualValuta.ExchangeRate = valuta.ExchangeRate;
+            }
         }
 
         public void AddValuta(Valuta valuta)
         {
-            valutas.Add(valuta);
+            lock (serviceLock)
+            {
+                valutas.Add(valuta);
+            }
         }
 
         private Valuta findValuta(string iso)
